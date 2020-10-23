3ad964fb2a91a91164e0f0646ec7c092e122cfd9
Alex Germann <agermann@trihydro.com>
2020-10-23 15:52:06 -0600

    Added longest consecutive subsequence solution

---
+++ LongestConsecutiveSubsequence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testInput = new int[] { 100, 4, 200, 1, 3, 2 };
+            //testInput = new int[] { 8, 100, 4, 200, 1, 3, 2, 5, 6, 7, 99, 101, 102, 103, 104, 106, 105, 109, 108, 107 };
+
+            var length = GetLongestSubsequenceLength(testInput);
+            Console.WriteLine($"Longest consecutive element sequence is length {length}");
+        }
+
+        public static int GetLongestSubsequenceLength(int[] input)
+        {
+            // Store array in hashset to lookup by value in O(1)
+            HashSet<int> hashes = new HashSet<int>(input);
+            var longCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentValue = input[i];
+                if (!hashes.Contains(currentValue - 1))
+                {
+                    // Preceding value not found, this is the start of a subsequence. Let's find the length of it
+                    var count = 0;
+                    while (hashes.Contains(currentValue))
+                    {
+                        count++;
+                        currentValue++;
+                    }
+                    // Update longest sequence count if this subsequence was longer
+                    longCount = Math.Max(longCount, count);
+                }
+            }
+            return longCount;
+        }
+    }
+}
